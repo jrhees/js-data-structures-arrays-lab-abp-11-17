0fd075fac2ee76d941403b2878c68d496131c25d
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-10 14:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const drivers = ['Milo', 'Otis', 'Garfield'];
 
-def destructivelyAppendDriver(name) {
-  return drivers.push(name);
-}
+//def destructivelyAppendDriver(name) {
+  //return drivers.push(name);
+//}
